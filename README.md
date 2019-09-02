9110a4ad797e7b14ab9802d4a629ada4cda85c39
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-09-02 12:30:29 +0300

    Setup resolvers

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# awesome-chuck
+# Awesome Chuck
 
 A serverless application that consumes the [Chuck Norris](https://api.chucknorris.io/) api and displays awesome jokes.
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,3 +1,27 @@
 export default {
+  Query: {
+    categories: (parent, args, context, info) => {
+      const { dataSources } = context;
+      return dataSources.chuckNorrisApi.getCategories();
+    },
 
+    randomJoke: (parent, args, context, info) => {
+      const { dataSources } = context;
+      return dataSources.chuckNorrisApi.getRandomJoke();
+    },
+
+    joke: (parent, args, context, info) => {
+      const { category } = args;
+      const { dataSources } = context;
+
+      return dataSources.chuckNorrisApi.getRandomJokeByCategory(category);
+    },
+
+    searchJoke: (parent, args, context, info) => {
+      const { queryString } = args;
+      const { dataSources } = context;
+
+      return dataSources.chuckNorrisApi.getSearchResult(queryString);
+    }
+  }
 };
