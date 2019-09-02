8dcefc642be98b7763e0c493be2d71b6caa93e2e
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-09-02 13:50:06 +0300

    Configure apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import App from './components/App';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// Set up our apollo-client to point at the server we created
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://4y8kwtqlm4.execute-api.us-east-2.amazonaws.com/dev/graphql'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
